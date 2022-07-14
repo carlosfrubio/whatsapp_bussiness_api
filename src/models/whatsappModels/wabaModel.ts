c563f737f8d68043b5598b99866ffb2a70fd0a64
ÂCarlos Rubio <carlosblondsk8@gmail.com>
2022-07-14 11:36:52 -0500

    commnets

--- src/models/whatsappModels/wabaModel.ts
+++ src/models/whatsappModels/wabaModel.ts
@@ -45,6 +45,3 @@ export const PhonesModel = model<IPhonesModel>(
   phonesModel,
   "phones"
 );
-
-
-//{  "_id": {    "$oid": "62cf6378920d4a605e7f8634"  },  "token": "EAAOaZChJH7hoBAJtMVnKFH3RvkfgIwWdZBCoWIG9bY0WgZBSb2Y2E91bI1dZA01xMsfDZCfYZClgQixBUn7sh2T6Kf38ZCOUWI23kHnioVi7vD5eWt4ubZBRZBRjTF8ZBcvxAYB1fNqq6Jt9VTroak1nlCJHyjZCDPqrDnyCOdqAwZBXTiu3y6lZBnbMIYZC51ZAvnwTYB0a6hiT9DV9jvkXyq6S2QajVK5uWPwss0ZD",  "bot_url": "https://rasaingredion.iaxon.co/webhooks/rest/webhook",  "phone_number": "573214384387",  "verify_token": "EAAOaZChJH7hoBAC3MNVBLfcpMEt9bSCFrk7Y8eVNUn0FDKbOWkzNNUU4PRI6Fjs4zEZBN1qIe0LMpQZBReZAZCSMA5JkZC7xe0uC5JSsQufEoJ3YYadiivWv8lzei5HCRPyccUt631r0qzN8G9tmEZCZBsjBJ7e7E5lgeGz921Rz7t4PPxYqLtu6qeSBd572rPESZAS3pOYuyaN5yqymftBhLnOi8X2bIZB5AZD",  "waba_id": "111720974938259",  "phone_number_id": "102876375835516"}
